d91d9e230ce8a653c0341f4e7b9e619bb05e5630
Rasa Hosseinzadeh <rasa.hosseinzadeh@gmail.com>
2022-07-09 22:18:54 -0400

    Bug Fix.

--- config.def.h
+++ config.def.h
@@ -116,7 +116,6 @@ static const char *colorname[] = {
 	"#ebdbb2",
 	[255] = 0,
 	/* more colors can be added after 255 to use with DefaultXX */
-<<<<<<< HEAD
 	"#add8e6", /* 256 -> cursor */
 	"#555555", /* 257 -> rev cursor*/
 	"#282a36", /* 258 -> bg */
@@ -130,7 +129,7 @@ static const char *colorname[] = {
  */
 unsigned int defaultfg = 259;
 unsigned int defaultbg = 258;
-static unsigned int defaultcs = 256;
+unsigned int defaultcs = 256;
 static unsigned int defaultrcs = 257;
 unsigned int bg = 258, bgUnfocused = 258;
 
